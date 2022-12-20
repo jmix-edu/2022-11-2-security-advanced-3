33fe53e2d1fbd5e41eba699d420ea078025bbcdf
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-12-20 20:16:28 +0400

    Complete

--- src/main/java/com/company/sociallogin/OAuthSecurityConfig.java
+++ src/main/java/com/company/sociallogin/OAuthSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.company.sociallogin;
 
+import com.company.sociallogin.entity.User;
 import com.company.sociallogin.security.FullAccessRole;
 import com.company.sociallogin.security.OAuth2UserPersistence;
 import io.jmix.core.JmixOrder;
@@ -14,8 +15,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
@@ -62,9 +65,20 @@ public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
      * Service responsible for loading OAuth2 users (GitHub uses OAuth2 protocol)
      */
     private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
         return (userRequest) -> {
-            // todo Implement user synchronization
-            return null;
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            Integer githubId = oAuth2User.getAttribute("id");
+            User user = oidcUserPersistence.loadUserByGithubId(githubId);
+            user.setUsername(oAuth2User.getName());
+            user.setGithubId(githubId);
+            user.setEmail(oAuth2User.getAttribute("email"));
+
+            user = oidcUserPersistence.saveUser(user);
+            user.setAuthorities(getDefaultGrantedAuthorities());
+
+            return user;
         };
     }
 
@@ -72,9 +86,20 @@ public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
      * Service responsible for loading OIDC users (Google uses OIDC protocol)
      */
     private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        OidcUserService delegate = new OidcUserService();
         return (userRequest) -> {
-            // todo Implement user synchronization
-            return null;
+            OidcUser oidcUser = delegate.loadUser(userRequest);
+
+            String googleId = oidcUser.getSubject();
+
+            User user = oidcUserPersistence.loadUserByGoogleId(googleId);
+            user.setUsername(googleId);
+            user.setGoogleId(googleId);
+            user.setEmail(oidcUser.getEmail());
+
+            user = oidcUserPersistence.saveUser(user);
+            user.setAuthorities(getDefaultGrantedAuthorities());
+            return user;
         };
     }
 
